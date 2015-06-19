de0447134d47738e41ee4a9267d5da9ec7c3b71b
Sean Overby <soverby@gmail.com>
2015-06-19 11:32:54 -0400

    Initial Commit

---
+++ SOCoreDataStack/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  SOCoreDataStackContainer
+//
+//  Created by Overby, Sean on 6/19/15.
+//  Copyright (c) 2015 Sean Overby. All rights reserved.
+//
+
+import Foundation
---
+++ SOCoreDataStack/SOCoreDataStack.h
@@ -0,0 +1,19 @@
+//
+//  SOCoreDataStack.h
+//  SOCoreDataStack
+//
+//  Created by Overby, Sean on 6/19/15.
+//  Copyright (c) 2015 Sean Overby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SOCoreDataStack.
+FOUNDATION_EXPORT double SOCoreDataStackVersionNumber;
+
+//! Project version string for SOCoreDataStack.
+FOUNDATION_EXPORT const unsigned char SOCoreDataStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOCoreDataStack/PublicHeader.h>
+
+
